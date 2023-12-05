16dc9672b4c35488731472d2eab21292748be753
jlmansilla <juanmancillaster@gmail.com>
2023-12-05 00:14:15 +0000

    modificandoo el development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+   # Permitir el host bloqueado
+  config.hosts << "4e0ea6ca042d4f67ada88e548e36825b.vfs.cloud9.us-east-1.amazonaws.com"
 end
