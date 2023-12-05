5d7980b722fd10267253ad13e556214c6712f85c
jlmansilla <juanmancillaster@gmail.com>
2023-12-05 00:51:45 +0000

    modificandoo root y applicattion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "!Hola, Mundo!"
+    render html: "¡Hola, Mundo!"
+  end
+
+  def goodbye
+    render html: "¡Adiós Mundo!"
   end
 end
